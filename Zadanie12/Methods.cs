ff027d6e51cd2156820b9f491133d7b3b423e57f
Łukasz Malicki <lukasz.malicki@pracuj.pl>
2019-09-09 11:19:23 +0200

    add excercise 1 and 2

---
+++ Zadanie12/Methods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Zadanie12
+{
+    public class Methods
+    {
+        private const double Pi = Math.PI;
+        private const float PromienKola1 = 10;
+        private const float PromienKola2 = 20;
+        private const float PromienKola3 = 50;
+
+        public void WypiszDane()
+        {
+            Console.WriteLine("Promień koła nr 1 = " + PromienKola1 + "\n" +
+                              "Promień koła nr 2 = " + PromienKola2 + "\n" +
+                              "Promień koła nr 3 = " + PromienKola3 + "\n");
+
+            Console.ReadKey();
+        }
+
+        public void PokazWyliczenia()
+        {
+            Console.WriteLine("Koło 1: Obwód = " + ObliczObwod(PromienKola1) +
+                              " Pole = " + ObliczPole(PromienKola1));
+
+            Console.WriteLine("Koło 2: Obwód = " + ObliczObwod(PromienKola2) +
+                              " Pole = " + ObliczPole(PromienKola2));
+
+            Console.WriteLine("Koło 3: Obwód = " + ObliczObwod(PromienKola3) +
+                              " Pole = " + ObliczPole(PromienKola3));
+        }
+
+        public double ObliczObwod(float promien)
+        {
+            return 2 * Pi * promien;
+        }
+
+        public double ObliczPole(float promien)
+        {
+            return Pi * Math.Pow(promien, 2);
+        }
+
+    }
+}
--- Zadanie12/Program.cs
+++ Zadanie12/Program.cs
@@ -1,21 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Zadanie12
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+        private static Methods _actions;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        private static void Main(string[] args)
+        {
+            _actions = new Methods();
+            _actions.WypiszDane();
+            _actions.PokazWyliczenia();
         }
     }
 }
